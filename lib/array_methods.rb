876d04e98532fd04fd79e6bcd382ba366a8f3c54
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-04 02:29:39 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,27 @@
 def using_include(array, element)
-
+  if array.include?(element)
+  return TRUE
+else 
+  return FALSE
+  end
 end
 
 def using_sort(array)
-
+  return array.sort
 end
 
 def using_reverse(array)
-
+  array.reverse
 end
 
 def using_first(array)
-
+  array.first
 end
 
 def using_last(array)
-
+  array.last
 end
 
 def using_size(array)
-
+  array.size
 end
